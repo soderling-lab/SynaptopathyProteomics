f664569c3ba35bc34e9b77e97d9113023dc64bb1
twesleyb <tyler.w.bradshaw@duke.edu>
2019-07-08 08:54:55 -0400

    updating gosim code

--- Code/temp-goSim.R
+++ Code/temp-goSim.R
@@ -1,9 +1,22 @@
 #!/usr/bin/env Rscript
 
+#-------------------------------------------------------------------------------
+# Prepare the workspace.
+#-------------------------------------------------------------------------------
+
+rm(list = ls())
+dev.off()
+cat("\014") # alternative is cat("\f")
+options(stringsAsFactors = FALSE)
+
 library(org.Mm.eg.db)
 library(GOSemSim)
 
-# Build GO database.
+#-------------------------------------------------------------------------------
+# Prepare to calculate GO semantic similarity between random communities. 
+#-------------------------------------------------------------------------------
+
+# Build GO databases.
 if (!exists("msGOMF")){ msGOMF <- godata('org.Mm.eg.db', ont= "MF")}
 if (!exists("msGOBP")){ msGOBP <- godata('org.Mm.eg.db', ont= "BP")}
 if (!exists("msGOCC")){ msGOCC <- godata('org.Mm.eg.db', ont= "CC")}
@@ -12,34 +25,43 @@ msGO <- list(msGOMF,msGOBP,msGOCC)
 names(msGO) <- c("MF","BP","CC")
 
 # Load randomly seeded communities from file. 
-if (generate_random_graphs == FALSE) {
-  print("Loading previously generated random communities!")
-  file <- "D:/Documents/R/Synaptopathy-Proteomics/RData/Random_Communities.RDS"
-  random_communities <- readRDS(file)
-}
+print("Loading previously generated random communities!")
+file <- "C:/Users/User/Documents/Tyler/Synaptopathy-Proteomics/RData/Random_Communities.RDS"
+random_communities <- readRDS(file)
+
+# Empty list for output of loop.
+gosim <- list()
+
+#-------------------------------------------------------------------------------
+# Loop to calculate GO semantic similarity. 
+#-------------------------------------------------------------------------------
 
 # Loop through all iterations, calculate GO semantic similarity between 
 # Shank2, Shank3, Syngap1, and Ube3a communties. 
-for (i in 1:length(random_communities)){
+n_iter = length(random_communities)
+for (i in start:n_iter){
   
   # Initialize progress bar.
-  if (i == 1) { 
+  if (i == start) { 
     print("Calculating GO semantic similarity between DEP communities...")
-    print("This may take several hours...")
+    print("This may take several days...")
     pb <- txtProgressBar(min=0, max = n_iter, style = 3) 
-  } else if (i != 1) {
+  } else if (i != start) {
     setTxtProgressBar(pb, i) }
   
   # Collect the data from random_communities list. 
   clusters <- random_communities[[i]]$combined_node
   
   # Evaluate GO semanitic similarity between DEP communities (clusters).
-  system.time({
-    gosim <- mclusterSim(clusters, semData=msGO$BP, measure="Wang", combine="BMA")
-  })
+  gosim[[i]] <- mclusterSim(clusters, semData=msGO$BP, measure="Resnik", combine="avg")
+
+  # Save progress every iteration...
+  file <- "C:/Users/User/Documents/Tyler/Synaptopathy-Proteomics/RData/goSim.RDS"
+  saveRDS(goSim, file)
   
   # When done, shut down progress bar.
-  if (i == n_iter) { close(pb) }
+  if (i == n_iter) { print("Complete!"); close(pb) }
 }
 
 #------------------------------------------------------------------------------
+
