d1120444dd4d0131a5b8a176ac25730ede17e8ce
tweleyb <tyler.w.bradshaw@duke.edu>
2019-11-18 15:36:51 -0500

    running on dcc- weak pres

--- analysis/3_Network-Analysis/4_compare-networks.R
+++ analysis/3_Network-Analysis/4_compare-networks.R
@@ -12,7 +12,7 @@
 
 # User parameters to change:
 stats <- c(1:7)        # Which module statistics (7) to use for perm testing.
-strength <- "strong"      # Preservation criterion: strong = all, weak = any sig stats.
+strength <- "weak"      # Preservation criterion: strong = all, weak = any sig stats.
 res <- c(1:100)        # Resolutions to be analyzed.
 cutoff <- 1            # Size cutoff to be a module.
 partition <- "6142226" # Which partition file to use as input?
