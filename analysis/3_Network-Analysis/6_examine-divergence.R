33bcf18909cfaab7b92d01e68d9fd0c97b792f11
twesleyb <tyler.w.bradshaw@duke.edu>
2019-11-26 09:24:35 -0500

    update

--- analysis/3_Network-Analysis/6_examine-divergence.R
+++ analysis/3_Network-Analysis/6_examine-divergence.R
@@ -18,9 +18,15 @@ suppressPackageStartupMessages({
   library(NetRep)
   library(getPPIs)
   library(igraph)
+  library(ggplot2)
+  library(ggdendro)
+  library(RCy3)
 })
 
 # Directories.
+if (rstudioapi::isAvailable()) {
+  setwd("D:/projects/SynaptopathyProteomics/analysis/3_Network-Analysis")
+}
 here <- getwd()
 root <- dirname(dirname(here))
 funcdir <- file.path(root, "R")
@@ -79,6 +85,18 @@ sft <- silently({
 })
 names(sft) <- c("wt","ko")
 
+# Networks.
+wtNet <- abs(wtAdjm^sft['wt'])
+koNet <- abs(koAdjm^sft['ko'])
+
+#d <- dist(1-wtNet) # euclidean distances between the rows
+#fit <- cmdscale(d,eig=TRUE, k=2) # k is the number of dim
+#fit # view results
+# plot solution
+#x <- fit$points[,1]
+#y <- fit$points[,2]
+#plot(x,y)
+
 # Load network comparison results.
 myfile <- list.files(rdatdir,pattern="6490667",full.names=TRUE)
 comparisons <- readRDS(myfile)
@@ -88,6 +106,12 @@ moduleChanges <- fread(list.files(rdatdir,pattern="Divergence.csv",full.names=TR
 
 # Fix column names.
 moduleChanges <- setNames(moduleChanges,gsub(" ","_",colnames(moduleChanges)))
+
+# Generate a plot.
+df <- moduleChanges %>% select(resolution,wt_nDivergent,ko_nDivergent) %>%
+	melt(id.vars = c("resolution"))
+plot <- ggplot(df,aes(x=resolution,y=value,colour=variable)) + 
+	geom_line() + geom_point()
 
 # Which resolutions have changes?
 x <- moduleChanges %>% filter(ko_nDivergent == 1) %>% 
@@ -126,7 +150,13 @@ for (i in 1:dim(x)[1]){
 }
 
 # Convert to distance matrix and then plot.
+hc = hclust(dist(dm))
+plot <- ggdendrogram(hc)
+ggsave(plot,file="dys_modules_dendro.tiff")
 
+# Two manin groups of proteins.
+# 1. P1-2-4-7-10-5-6
+# 2. P3-9-11-8-13-12-14 (overlap among these proteins is much lower.)
 
 #------------------------------------------------------------------------------
 # Prepare ppi graph.
@@ -143,18 +173,21 @@ prots <- colnames(wtAdjm)
 entrez <- protmap$entrez[match(prots,protmap$ids)]
 
 # Build a graph with all proteins.
-graph <- buildNetwork(ppis, entrez, taxid = 10090)
+g <- buildNetwork(ppis, entrez, taxid = 10090)
 
 #------------------------------------------------------------------------------
-# Examine ~optimal resolution.
+# Examine ~optimal resolutions.
 #------------------------------------------------------------------------------
+resolutions <- c(29, 35, 36, 40, 41, 42, 44, 45, 48, 49, 55, 58, 66, 79)
+send_to_cytoscape <- FALSE
+changes_df <- list()
 
-#c(29, 35, 36, 40, 41, 42, 44, 45, 48, 49, 55, 58, 66, 79)
-res <- 58 # best GO = 29, biggest with with dysregulate module = 79
+for (r in seq_along(resolutions)){
+  # Collect data from resolution of interest.
+res <- resolutions[r]
+message(paste("Resolution:",res))
 GO <- koAllGO
-geno <- "ko"
-###
-#examinePartition <- function(comparisons,res,geno,GO){
+geno <- "ko" # Working with ko partitions... we only see divergence in KO modules.
 allDat <- comparisons[[res]]
 namen <- names(allDat)[grep(geno,names(allDat))]
 data <- allDat[namen]
@@ -163,8 +196,15 @@ nModules <- length(modules)
 changes <- sapply(modules,unique)
 divergent <- modules[names(changes[changes=="divergent"])]
 goDat <- GO[[res]]
-myfile <- file.path(tabsdir,paste0("3_",toupper(geno),"_R",res,"_Module_GO_enrichment.xlsx"))
-write_excel(goDat,myfile)
+# Percent variance explained by a module.
+part <- data[[1]]
+wtME <- moduleEigengenes(wtDat, colors=part, impute = FALSE, softPower = sft['wt'])
+koME <- moduleEigengenes(koDat, colors=part, impute = FALSE, softPower = sft['ko'])
+wtPVE <- wtME$varExplained
+koPVE <- koME$varExplained
+names(wtPVE) <- names(koPVE) <- names(modules)
+#myfile <- file.path(tabsdir,paste0("3_",toupper(geno),"_R",res,"_Module_GO_enrichment.xlsx"))
+#write_excel(goDat,myfile)
 # for ease, fix names.
 names(goDat) <- names(modules) 
 # Sizes of divergent modules.
@@ -173,7 +213,6 @@ message(paste("Divergent module name :",names(divergent)))
 message(paste("Number of divergent modules:",length(divergent)))
 message(paste("Number of proteins in divergent module:",length(unlist(divergent))))
 #}
-
 # Check correlation coefficients.
 getAdjm <- function(module,adjm){
 	idx <- idy <- match(names(module),rownames(adjm))
@@ -182,20 +221,72 @@ getAdjm <- function(module,adjm){
 }
 subWT <- lapply(divergent,function(x) getAdjm(x,wtAdjm))
 subKO <- lapply(divergent,function(x) getAdjm(x,koAdjm))
+# Rewired proteins:
+df <- as.data.frame(cbind(melt(subWT[[1]]),ko=melt(subKO[[1]])$value))
+colnames(df)[3] <- "wt"
+df$delta <- df$ko-df$wt
+df <- df[order(df$delta,decreasing=TRUE),]
 # Mean edge strength
-sapply(subKO,mean)
-sapply(subWT,mean)
-
+#sapply(subKO,mean)
+#sapply(subWT,mean)
 # But are they connected?
 prots <- names(divergent[[1]])
 entrez <- protmap$entrez[match(prots, protmap$ids)]
-subg <- induced_subgraph(graph,entrez)
-length(V(subg))
-length(E(subg))
-
-# How many sig?
-sum(prots %in% sigProts)
-
+subg <- induced_subgraph(g,entrez)
 # From which genos?
 subdat <- melt(subset(glmDat,rownames(glmDat) %in% prots),id.vars="Uniprot")
-subdat %>% group_by(variable) %>% summarize(sum(value<0.05))
+sigdf <- subdat %>% group_by(variable) %>% summarize(sum(value<0.05))
+colnames(sigdf)[2] <- "value"
+# Any enrichment?
+df <- melt(glmDat,id.vars="Uniprot")
+df <- df %>% group_by(variable) %>% summarize(nSig=sum(value<0.05))
+df$Freq <- df$nSig/dim(glmDat)[1]
+sigdf$expected <- length(prots) * df$Freq
+
+# Hypergeometric p-value.
+sigdf$pval <- phyper(sigdf$value-1, # sample success
+       df$nSig,                     # pop_success
+       dim(glmDat)[1] - df$nSig,    # pop - pop_success
+       rep(length(prots),length(sigdf$value)), # sample size
+       lower.tail = FALSE) 
+sigdf$fdr <- p.adjust(sigdf$pval,"bonferroni")
+changes_df[[r]] <- sigdf
+# Add sigprot vertex attribute.
+names(sigProts) <- protmap$entrez[match(sigProts,protmap$ids)]
+sig <- names(V(subg)) %in% names(sigProts)
+subg <- set_vertex_attr(subg, "sigProt",value = sig)
+# Switch node names.
+subg <- set_vertex_attr(subg, "name", index = V(subg), vertex_attr(subg,"symbol"))
+namen <- paste0("R",res)
+# Send to cytoscape.
+if (send_to_cytoscape){
+cytoscapePing()
+createNetworkFromIgraph(subg,namen)
+}
+#length(V(subg))
+message(paste("Number of PPIs among nodes:",length(E(subg))))
+# How many sig?
+message(paste("Number of sig protiens:",sum(prots %in% sigProts)))
+# How many Psm*
+message(paste("Number of proteasome proteins:",sum(grepl("Psm*",prots))))
+message("\n")
+}
+
+# Should collect all sig DA prots from module of interst....
+
+# ANy resolutions with multiple significant genotypes?
+which_res <- sapply(changes_df,function(x) as.character(subset(x,x$fdr<0.05)$variable))
+names(which_res) <- paste0("R",resolutions)
+which_res[paste0("R",c(resolutions[sapply(which_res,length)>1]))]
+
+# Are the proteins up or down? Which are the hubs?
+# Can visualize "rewiring of module"
+
+# make in pie chart.
+# Barplot
+pie <- ggplot(df, aes(x="", y=value, fill=variable))+
+	geom_bar(width = 1, stat = "identity") + 
+	coord_polar("y", start=0)
+myfile <- paste0("R",res,"_pie.tiff")
+ggsave(pie,file=myfile)
+
