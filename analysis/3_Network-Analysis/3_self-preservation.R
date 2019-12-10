caf857fec30cb2e4f1bccc6862c91a6127a35e30
tweleyb <tyler.w.bradshaw@duke.edu>
2019-12-10 12:45:10 -0500

    analyzing self-pres of cortex on dcc

--- analysis/3_Network-Analysis/3_self-preservation.R
+++ analysis/3_Network-Analysis/3_self-preservation.R
@@ -35,7 +35,7 @@
 stats <- c(1:7) # Module statistics to use for permutation testing.
 strength <- "strong" # Criterion for preservation: strong = ALL, weak = ANY sig stats.
 weighted <- FALSE # Weighted or unweighted. If TRUE, then appropriate soft-power will be calculated.
-self <- "Striatum" # Which networks to test self preservation in? #self = c("wt","ko","cortex","striatum","combined")
+self <- "Cortex" # Which networks to test self preservation in? #self = c("wt","ko","cortex","striatum","combined")
 nres <- 100 # Total number of resolutions to be anlyzed.
 
 # Is this a slurm job?
