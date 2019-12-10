f471cf32f58bee1bc3fe8680f45583eb2e0071e9
twesleyb <tyler.w.bradshaw@duke.edu>
2019-12-10 12:52:15 -0500

    added header to filter_modules funciton

--- R/filter_modules.R
+++ R/filter_modules.R
@@ -1,5 +1,22 @@
-# Remove small modules.
-filter_modules <- function(partition, cutoff = 5) {
+#' filter_modules
+#'
+#' remove small modules from partition
+#'
+#' @param partition - named vector 
+#'
+#' @return none
+#'
+#' @author Tyler W Bradshaw, \email{tyler.w.bradshaw@duke.edu}
+#'
+#' @references none
+#'
+#' @keywords none
+#'
+#' @export
+#'
+#' @examples
+#'filter_modules(partition,cutoff=5)
+filter_modules <- function(partition, cutoff = 1) {
 modules <- split(partition, partition)
 out <- names(modules)[sapply(modules, function(x) table(x) < cutoff)]
 partition[partition %in% out] <- 0
