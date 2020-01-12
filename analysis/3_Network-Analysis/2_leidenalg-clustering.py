b4c06196662dd62963406ce8cc4ae56804b3363b
twesleyb <tyler.w.bradshaw@duke.edu>
2020-01-12 17:24:01 -0500

    updating leidenalg clustering script to be more concise, pass dictionary to find_partition function

--- analysis/3_Network-Analysis/2_leidenalg-clustering.py
+++ analysis/3_Network-Analysis/2_leidenalg-clustering.py
@@ -1,60 +1,58 @@
 #!/usr/bin/env python3
 ' Clustering of the protein co-expression graph with Leidenalg.'
 
+import sys
+from sys import stderr
+
 ## User parameters: 
-input_adjm = "3_PPI_Adjm.csv" # Input adjacency matrix.
-#input_adjm = "3_GO_Semantic_Similarity_RMS_Adjm.csv"
-output_name = "PPI" # Output filename.
-method = "ModularityVertexPartition" 
+myfile = 0
+method = 0 # See methods and details below.
 
-## Resolution range for multiresolution methods:
-rmin = 0 # Min resolution.
-rmax = 1 # Max resolution. 
-nsteps = 100 # Number of steps.
+## Input adjacency matrix.
+input_adjm = ["3_PPI_Adjm.csv","3_GO_Semantic_Similarity_RMS_Adjm.csv"][myfile]
 
-# For PPI graph use SurpriseVertexPartition -- this minimizes the percent
-# unclustered while maximizing the number of modules.
-# For GO graph use CPM with resolution parameter = 1.0. This minimizes the
-# percent unclustered while retaining a large number of modules.
+## Leidenalg supports the following methods for optimization methods:
+methods = {
+        # Modularity
+        0: {'partition_type' : 'ModularityVertexPartition', 
+            'weights' : 'positive',
+            'resolution_parameter' : None},
+        # Suprise
+        1: {'partition_type' : 'SupriseVertexPartition', 
+            'weights' : 'positive',
+            'resolution_parameter' : None},
+        # RBConfig
+        2: {'partition_type' : 'RBConfigurationVertexPartition', 
+            'weights' : 'positive',
+            'resolution_parameter' : {'start':0,'stop':1,'num':100}},
+        # RBEVertex
+        3: {'partition_type' : 'RBEVertexPartition', 
+            'weights' : 'positive',
+            'resolution_parameter' : {'start':0,'stop':1,'num':100}},
+        # CPM
+        4: {'partition_type' : 'CPMVertexPartition', 
+            'weights' : 'weighted',
+            'resolution_parameter' : {'start':0,'stop':1,'num':100}},
+        # Significance
+        5: {'partition_type' : 'SignificanceVertexPartition', 
+            'weights':None,
+            'resolution_parameter' : None}}
 
-## Methods: Leidenalg supports the following methods for optimization
-#           of community detection:
-#
-# Methods for positive edge weights, no resolution parameter:
-# [1] ModularityVertexPartition -- Implements modularity. Only 
-#     well-defined for positive edge weights. No resolution parameter.
-# [2] SurpriseVertexPartition -- Implements (asymptotic) Surprise. 
-#     This quality function is well-defined only for positive edge 
-#     weights. No resolution parameter.
-#
-# Methods for positive edges with linear resolution parameter:
-# [3] RBConfigurationVertexPartition -- Implements Reichardt and 
-#     Bornholdt’s Potts model with a configuration null model. 
-#     Only well-defined for positive edge weights.
-# [4] RBERVertexPartition -- Implements Reichardt and Bornholdt’s 
-#     Potts model with a configuration null model. 
-#     Only well-defined for positive edge weights. 
-#
-# Methods for positive and negative edge weights:
-# [5] CPMVertexPartition -- Implements CPM. Quality function is 
-#     well-defined for both positive and negative edge weights. 
-#     Can utilize a linear resolution parameter.
-#
-# Method for unweighted graphs without resolution parameter:
-# [6] SignificanceVertexPartition -- Implements Significance. This 
-#     quality function is well-defined only for unweighted graphs.
-#     Does not utilize a resolution parameter, but tries to find
-#     the best resolution/partition.
+# Parameters for clustering.
+parameters = methods.get(method)
+method = parameters.get('partition_type')
+
+# Status report.
+print("Performing Leidenalg clustering utilizing the {}".format(method),
+        "method to find optimal partition(s).", file=stderr)
 
 #------------------------------------------------------------------------------
 ## Prepare the workspace.
 #------------------------------------------------------------------------------
 
-import sys
 import os
-import glob
 from os.path import dirname
-from sys import stderr
+import glob
 
 # Directories.
 here = os.getcwd()
@@ -75,97 +73,82 @@ jobID = myfun.xstr(envars['SLURM_JOBID'])
 ## Load input adjacency matrix and create an igraph object.
 #------------------------------------------------------------------------------
 
-from pandas import read_csv
+from pandas import read_csv, DataFrame
 from igraph import Graph
 
-# Read bicor adjacency matrix as input.
+# Read bicor adjacency matrix.
 myfile = os.path.join(datadir,input_adjm)
 adjm = read_csv(myfile, header = 0, index_col = 0)
 adjm = adjm.set_index(keys=adjm.columns) # Add row names.
 
+# Incorporate weighted, positive, unweighted graph types.
+if parameters['weights'] is 'positive':
+    A = abs(adjm.values)
+elif parameters['weights'] is 'weighted':
+    A = adjm.values
+
 # Create igraph object.
-A = adjm.values
-g = Graph.Adjacency((A > 0).tolist())
+g = Graph.Adjacency(A.tolist())  #g = Graph.Adjacency((A > 0).tolist()) # Unweighted or positive?
 g.es['weight'] = A[A.nonzero()]
 g.vs['label'] = adjm.columns
 
+# Update weights parameter.
+parameters['weights'] = 'weight'
+
 # Remove self-loops.
 g = g.simplify(multiple = False, loops = True)
+
+# Add graph to input parameters for clustering.
+parameters['graph'] = g
 
 #------------------------------------------------------------------------------
 ## Community detection with the Leiden algorithm.
 #------------------------------------------------------------------------------
 
 from numpy import linspace
-from leidenalg import Optimiser
-from leidenalg import find_partition
+from leidenalg import Optimiser, find_partition
 from progressbar import ProgressBar
 from importlib import import_module
 
-# Dynamically load the partition_type class--the clusering optimization method.
-partition_type = getattr(import_module('leidenalg'), method)
+# Update partition type parameter.
+# Dynamically load the partition_type class. This is the method to be used for
+# clusering optimization.
+parameters['partition_type'] = getattr(import_module('leidenalg'),method)
 
-# Methods that don't support multi-resolution clustering:
-out = ["ModularityVertexPartition", 
-        "SurpriseVertexPartition",
-        "SignificanceVertexPartition"]
-# Check if users optimization method supports resolution parameter. 
-single_resolution = myfun.contains(out,method)
+# Update n_iterations parameter.
+parameters['n_iterations'] = -1
+
+# Remove any None type parameters.
+out = [key for key in parameters if parameters.get(key) is None]
+for key in out: del parameters[key]
 
 # Perform Leidenalg community detection. 
-if (single_resolution):
-    # Single resolution clustering methods:
+if parameters.get('resolution_parameter') is None:
+    # Single resolution methods.
     profile = list()
-    if weighted:  
-        # Analysis of weighted graph at single resolution.
-        partition = find_partition(g, partition_type, weights='weight')
-        optimiser = Optimiser()
-        diff = optimiser.optimise_partition(partition,n_iterations=-1)
-        partition = myfun.filter_modules(partition)
-        profile.append(partition)
-    else:
-        # Analysis of unweighted graph at single resolution.
-        partition = find_partition(g, partition_type)
-        optimiser = Optimiser()
-        diff = optimiser.optimise_partition(partition,n_iterations=-1)
-        partition = myfun.filter_modules(partition)
-        profile.append(partition)
+    partition = find_partition(**parameters)
+    optimiser = Optimiser()
+    diff = optimiser.optimise_partition(partition,n_iterations=-1)
+    partition = myfun.filter_modules(partition)
+    profile.append(partition)
 else:
-    # Loop to perform multi-resolution clustering:
-    print("Performing Leiden algorithm clustering of the" 
-        " protein co-expression network.\n", file = stderr)
+    # Loop to perform multi-resolution clustering methods.
     pbar = ProgressBar()
-    resolution_range = linspace(rmin,rmax,nsteps)
     profile = list()
+    resolution_range = linspace(**parameters.get('resolution_parameter'))
     for resolution in pbar(resolution_range):
-        # Perfrom La clustering.
-        if weighted:
-            # Analysis of weighted graph.
-            partition = find_partition(g, partition_type, 
-                    weights='weight', resolution_parameter=resolution)
-            optimiser = Optimiser()
-            diff = optimiser.optimise_partition(partition,n_iterations=-1)
-            partition = myfun.filter_modules(partition)
-            profile.append(partition)
-        else:
-            # Analysis of unweighted graph.
-            partition = find_partition(g, partition_type, 
-                    resolution_parameter=resolution)
-            optimiser = Optimiser()
-            diff = optimiser.optimise_partition(partition,n_iterations=-1)
-            partition = myfun.filter_modules(partition)
-            profile.append(partition)
+        parameters['resolution_parameter'] = resolution
+        partition = find_partition(**parameters)
+        optimiser = Optimiser()
+        diff = optimiser.optimise_partition(partition,n_iterations=-1)
+        partition = myfun.filter_modules(partition)
+        profile.append(partition)
         # Ends loop.
 # Ends If/else.
-
-print(f"Complete! Examined network at {len(profile)} resolutions!", 
-        file = stderr)
 
 #------------------------------------------------------------------------------
 ## Save clustering results.
 #------------------------------------------------------------------------------
-
-from pandas import DataFrame
 
 # Collect partition results and save as csv. 
 if len(profile) is 1:
@@ -183,6 +166,7 @@ else:
         'Resolution' : [partition.resolution_parameter for partition in profile]}
 
 # Save cluster membership vectors.
+output_name = input_adjm.split("_")[1]
 myfile = os.path.join(datadir, jobID + "3_" + output_name + "_" + 
         method + "_partitions.csv")
 DataFrame(results['Membership']).to_csv(myfile)
@@ -192,7 +176,3 @@ df = DataFrame.from_dict(results)
 myfile = os.path.join(datadir, jobID + "3_" + output_name + "_" + 
         method + "_profile.csv")
 df.to_csv(myfile)
-
-
-
-
