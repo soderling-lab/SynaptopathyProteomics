260730ccaf86ec9d6bd0708f1c51b83b730c8ad4
twesleyb <tyler.w.bradshaw@duke.edu>
2020-02-02 20:29:46 -0500

    parallelizing mcl clustering

--- analysis/3_Network-Analysis/3_parallel_ne_mcl_clustering.R
+++ analysis/3_Network-Analysis/3_parallel_ne_mcl_clustering.R
@@ -62,88 +62,16 @@ all_partitions <- lapply(seq(nrow(partitions)),function(x) {
 # Create a Co-expression graph.
 g0 <- graph_from_adjacency_matrix(adjm,mode="undirected",weighted=TRUE)
 
-## Loop through resolutions, for each module get best mcl partition.
-results <- list()
-
-# Function to do mcl clustering for a given resolution.
-cluster_mcl <- function(resolution) {
-suppressPackageStartupMessages({
-	library(neten)
-	library(igraph)
-	library(parallel)
-	library(doParallel)
-})
-	#message(paste("\nWorking on resolution",resolution,"..."))
-	part <- all_partitions[[resolution]]
-	modules <- split(part,part)
-	module_sizes <- table(part)
-	too_big <- names(module_sizes)[module_sizes > max_size]
-	#message(paste0("Breaking down (",length(too_big),") large communities."))
-	mcl_partitions <- list()
-	## Loop through big modules, bet best MCL partition.
-	for (m in too_big){
-		#message(paste("... Working on module",m,"..."))
-		# Get subgraph.
-		prots <- names(modules[[m]])
-		g1 <- induced_subgraph(g0,vids=V(g0)[match(prots,names(V(g0)))])
-		# Network Enhancement.
-		g_ne <- neten(g1)
-		## Loop to sample inflation space.
-		output <- list()
-		for (i in seq_along(inflation)){
-			# MCL clustering.
-			mcl_part <- clusterMCL(g_ne,weight="weight",inflation[i])
-			# Modularity of re-weighted (NE) graph.
-			q <- modularity(g_ne,membership=mcl_part[names(V(g_ne))],
-					weights=abs(edge_attr(g_ne,'weight')))
-			# Summary stats.
-			k <- sum(names(table(mcl_part)) != "0")
-			output[[i]] <- list("Partition"=mcl_part,
-					    "Inflation"=inflation[i],
-					    "Clusters"=k,
-					    "Modularity"=q)
-		} # Ends loop through inflation space.
-		# Get ~best partition.
-		Q <- sapply(output,function(x) x$Modularity)
-		K <- sapply(output,function(x) x$Clusters)
-		parts <- lapply(output,function(x) x$Partition)
-		idx <- which(Q==max(Q))
-		best_q <- Q[idx]
-		best_i <- inflation[idx]
-		best_k <- K[idx]
-		# Status.
-		#message(paste("... ... Inflation:", best_i))
-		#message(paste("... ... Clusters :", best_k))
-		#message(paste("... ... Modularity:",round(best_q,3)))
-		# return best mcl partition.
-		mcl_partitions[[m]] <- parts[[idx]]
-	} # Ends loop through modules.
-	# Collect best mcl partitions.
-	return(mcl_partitions)
-} # Ends function.
-
-# Serial execution:
-#t0 <- Sys.time()
-#results <- foreach(resolution=seq(1,2)) %do% {
-#	cluster_mcl(resolution)
-#}
-#t1 <- Sys.time()
-#dt <- t1-t0
-#message(paste("Time to execute in series:",dt))
-
 # Register parallel clusters.
 workers <- makeCluster(c(rep("localhost",nThreads)),type="SOCK")
 registerDoParallel(workers)
 
 # Parallel execution:
-t0 <- Sys.time()
-results <- foreach(resolution=seq(1,2)) %dopar% {
+results <- foreach(resolution=resolutions) %dopar% {
 	cluster_mcl(resolution)
 }
-t1 <- Sys.time()
-dt <- t1-t0
-message(paste("Time to execute in parallel:",dt))
 
+# Shut down nodes.
 suppressWarnings(stopCluster(workers))
 
 # Loop to combine mcl partitions at every resolution.
