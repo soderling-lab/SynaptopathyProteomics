14b53e9814ec53410418100595ec5dbc44a556d3
twesleyb <twesleyb10@gmail.com>
2020-05-28 20:56:31 -0400

    foobar

--- analysis/3_Network-Analysis/0_run-analysis.sh
+++ analysis/3_Network-Analysis/0_run-analysis.sh
@@ -5,7 +5,7 @@
 # Check if an argument was passed.
 if [ $# -eq 0 ]
 then
-	echo "Error: Provide tissue type for analysis: 'Cortex' or 'Striatum'."
+	echo "ERROR: Provide tissue type for analysis: 'Cortex' or 'Striatum'."
 	exit
 fi
 
@@ -34,21 +34,75 @@ fi
 echo "Clustering the "$TISSUE" protein co-variation network."
 ./2_Leiden-clustering.py "$TISSUE" &> 02_"$TISSUE"_Leiden-clustering.txt
 
+# Check if completed successfully?
+if [ $? -eq 0 ]
+then
+	echo Passed.
+else
+	echo Failed.
+	exit
+fi
+
 # STEP 3.
 echo "Enforcing  "$TISSUE" module self-preservation."
 ./3_Module-preservation.R "$TISSUE" &> 03_"$TISSUE"_Module-preservation.txt
+
+# Check if completed successfully?
+if [ $? -eq 0 ]
+then
+	echo Passed.
+else
+	echo Failed.
+	exit
+fi
 
 # STEP 4.
 echo "Checking "$TISSUE" modules for convergent changes."
 ./4_Network-analysis.R "$TISSUE" &> 04_"$TISSUE"_Network-analysis.txt
 
+# Check if completed successfully?
+if [ $? -eq 0 ]
+then
+	echo Passed.
+else
+	echo Failed.
+	exit
+fi
+
 # STEP 5.
 echo "Analyzing "$TISSUE" modules for GO enrichment."
 ./5_Module_GO-Enrichment.R "$TISSUE" &> 05_"$TISSUE"_Module_GO-Enrichment.txt
+
+# Check if completed successfully?
+if [ $? -eq 0 ]
+then
+	echo Passed.
+else
+	echo Failed.
+	exit
+fi
 
 # STEP 6.
 echo "Testing "$TISSUE" modules for enrichment of DBD-associated genes."
 ./6_Module_DBD-Enrichment.R "$TISSUE" &> 06_"$TISSUE"_Module-DBD-Enrichment.txt
 
+# Check if completed successfully?
+if [ $? -eq 0 ]
+then
+	echo Passed.
+else
+	echo Failed.
+	exit
+fi
+
 # STEP 7.
 #./7_Select_Module_Analysis.R "$TISSUE" &> 07_"$TISSUE"_Module-DBD-Enrichment.txt
+
+# Check if completed successfully?
+if [ $? -eq 0 ]
+then
+	echo Passed.
+else
+	echo Failed.
+	exit
+fi
