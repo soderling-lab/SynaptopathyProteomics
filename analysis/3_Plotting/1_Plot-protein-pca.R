5917a539a72170f0810fb54b362c4d5706b6a3a8
twesleyb <twesleyb10@gmail.com>
2020-06-01 08:24:15 -0400

    update

--- analysis/3_Plotting/1_Plot-protein-pca.R
+++ analysis/3_Plotting/1_Plot-protein-pca.R
@@ -1,12 +1,15 @@
 #!/usr/bin/env Rscript
 
 ## Inputs:
-#analysis_type = "Cortex"
-analysis_type = "Striatum"
+tissue = "Cortex"
+
+#---------------------------------------------------------------------
+## Prepare the workspace.
+#---------------------------------------------------------------------
 
 # Load renv.
 root <- getrd()
-renv::load(root)
+renv::load(root,quiet=TRUE)
 
 # Imports:
 suppressPackageStartupMessages({
@@ -20,19 +23,12 @@ suppressWarnings({ devtools::load_all() })
 # Directory for saving figures:
 figsdir <- file.path(root,"figs")
 
-# Load protein data and graph partitions:
-expr_data <- paste0(tolower(analysis_type),"_data")
-part_data <- paste0(tolower(analysis_type),"_partition")
+# Dynamically load protein data and graph partition.
+expr_data <- paste0(tolower(tissue),"_data")
+part_data <- paste0(tolower(tissue),"_partition")
 data(list=c(expr_data,part_data))
-
-# Get the data we are gonna analyze:
-if (analysis_type == "Cortex") {
-	data <- cortex_data$Data
-	partition <- cortex_partition
-} else {
-	data <- striatum_data$Data
-	partition <- striatum_partition
-}
+eval(parse(text=paste0("data=",expr_data)))
+eval(parse(text=paste0("partition=",part_data)))
 
 # NOTE: Apply is done row-wise (dim = 1), but the result is transposed.
 # See://stackoverflow.com/questions/9521260/
@@ -40,5 +36,8 @@ dm_norm <- t(apply(2^data, 1, function(x) x/sum(x)))
 plot <- ggplotPCAprot(dm_norm,scale=TRUE,center=TRUE)
 
 # Save the plot.
-myfile <- file.path(figsdir,paste0(analysis_type,"_Protein_PCA.pdf"))
+myfile <- file.path(figsdir,paste0(tissue,"_Protein_PCA.pdf"))
 ggsave(file=myfile,plot,width=6,height=6)
+
+# Add colors.
+
